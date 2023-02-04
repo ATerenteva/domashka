9b74d20f5ea34ba2d9a34ed0f31c34fce3755451
ATerenteva <ATerenteva@inno.tech>
2023-02-04 22:03:26 +0300

    added a unit tests

---
+++ src/main/java/domashka/student/Main.java
@@ -0,0 +1,7 @@
+package domashka.student;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/domashka/student/GradeSource.java
@@ -0,0 +1,9 @@
+package domashka.student;
+
+import java.util.List;
+
+public class GradeSource {
+    public static List<Integer> ints() {
+        return List.of(0, 1, 6, 7);
+    }
+}
